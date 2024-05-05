08bbaa2a3f937c15d3f30f691c5932388d612bba
Sujith Thriumalaisamy <sujithmasi1267@gmail.com>
2024-05-06 01:25:29 +0530

    The Cleaner is working perfectly fine

--- exec/cleaner/src/index.ts
+++ exec/cleaner/src/index.ts
@@ -1,20 +1,14 @@
-const k8s = require("@kubernetes/client-node");
+import { KubeConfig, AppsV1Api } from "@kubernetes/client-node";
 import { createClient } from "redis";
-const kc = new k8s.KubeConfig();
+const kc = new KubeConfig();
 kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
+const k8sApi = kc.makeApiClient(AppsV1Api);
 const redis = createClient();
-setInterval(async () => {
-  const deploymentName: string =
-    (await redis.RPOP("deployments_to_be_deleted")) || "";
-  delete_deployment(deploymentName);
-}, 1000);
 
 function delete_deployment(deploymentName: string) {
-  const deleteOptions = new k8s.V1DeleteOptions();
   k8sApi
-    .deleteNamespacedDeployment(deploymentName, "default", deleteOptions)
+    .deleteNamespacedDeployment(deploymentName, "default")
     .then(() => {
       console.log(`Deployment ${deploymentName} deleted successfully`);
     })
@@ -22,3 +16,17 @@ function delete_deployment(deploymentName: string) {
       console.error(`Error deleting deployment ${deploymentName}: ${err}`);
     });
 }
+async function main() {
+  await redis.connect();
+  setInterval(async () => {
+    const deploymentName: string =
+      (await redis.RPOP("deployments_to_be_deleted")) || "";
+    console.log(deploymentName.length);
+    if (deploymentName.length === 0) {
+      //How to make this wait for few seconds if there is no Deployments
+    } else {
+      delete_deployment(deploymentName);
+    }
+  }, 1000);
+}
+main();
