852a7d061ab680d30014f7915b44e1d5e54e3ff0
Sujith Thriumalaisamy <sujithmasi1267@gmail.com>
2024-05-16 01:16:31 +0530

    Added testcases deployment route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import db from "@repo/db";
 import { createClient } from "redis";
 import { SubmissionStatus } from "@prisma/client";
-import fs from "fs";
+import { Octokit } from "octokit";
 
 require("dotenv").config();
 const redisClient = createClient({
@@ -84,9 +84,25 @@ app.post("/testcase/:id", async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const { testcase } = req.body;
-    fs.writeFileSync(`/shared/nfs/${id}.json`, testcase);
-    if (!id) return;
-    redisClient.LPUSH("testcases_to_be_created", id);
+    const octokit = new Octokit({
+      auth: process.env.TOKEN,
+    });
+    const stringifiedTestCase = JSON.stringify(testcase);
+    const base64Encoded = Buffer.from(stringifiedTestCase).toString("base64");
+    await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+      owner: process.env.REPO_OWNER || "",
+      repo: process.env.REPO || "",
+      path: `testcases/${id}.json`,
+      message: `Auto Commit: Added ${id}.json`,
+      committer: {
+        name: "Auto Commiter",
+        email: "hello@isujith.dev",
+      },
+      content: base64Encoded,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
     return res
       .status(200)
       .json({ message: "Auto Deployment for testcase Triggered!" });
